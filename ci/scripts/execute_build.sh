4581a2b53352439fe542b8547e9ee8e0d6434813
Sean Goller <sean@goller.net>
2022-04-01 15:03:10 -0700

    Add execute_build script.

---
+++ ci/scripts/execute_build.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -ex
+
+BASE_DIR=$(pwd)
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+if [[ -z "${GRADLE_TASK}" ]]; then
+  echo "GRADLE_TASK must be set. exiting..."
+  exit 1
+fi
+
+ROOT_DIR=$(pwd)
+BUILD_DATE=$(date +%s)
+
+DEFAULT_GRADLE_TASK_OPTIONS="--console=plain --no-daemon"
+
+SSHKEY_FILE="instance-data/sshkey"
+SSH_OPTIONS="-i ${SSHKEY_FILE} -o ConnectionAttempts=60 -o StrictHostKeyChecking=no"
+
+INSTANCE_IP_ADDRESS="$(cat instance-data/instance-ip-address)"
+
+SET_JAVA_HOME="export JAVA_HOME=/usr/lib/jvm/bellsoft-java${JAVA_BUILD_VERSION}-amd64"
+
+GRADLE_COMMAND="./gradlew \
+    ${DEFAULT_GRADLE_TASK_OPTIONS} \
+    clean build"
+
+echo "${GRADLE_COMMAND}"
+ssh ${SSH_OPTIONS} geode@${INSTANCE_IP_ADDRESS} "set -x  && mkdir -p tmp && cd spring-data-gemfire && ${SET_JAVA_HOME} && ${GRADLE_COMMAND}"
