7b53ec674ce599a8b040a975266ebe56b621787a
Udo Kohlmeyer <udo@vmare.com>
2023-01-24 13:33:30 +1100

    Updating transient dependencies

--- spring-data-vmware-gemfire/src/main/java/org/springframework/data/gemfire/GemfireUtils.java
+++ spring-data-vmware-gemfire/src/main/java/org/springframework/data/gemfire/GemfireUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) VMware, Inc. 2022. All rights reserved.
+ * Copyright (c) VMware, Inc. 2022-2023. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 package org.springframework.data.gemfire;
@@ -14,8 +14,9 @@ import org.springframework.data.gemfire.util.RegionUtils;
 import org.springframework.util.ClassUtils;
 
 /**
- * {@link GemfireUtils} is an abstract utility class encapsulating common functionality for accessing features
- * and capabilities of Apache Geode based on version as well as other configuration meta-data.
+ * {@link GemfireUtils} is an abstract utility class encapsulating common functionality for
+ * accessing features and capabilities of Apache Geode based on version as well as other
+ * configuration meta-data.
  *
  * @author John Blum
  * @see CacheFactory
@@ -25,102 +26,91 @@ import org.springframework.util.ClassUtils;
  */
 @SuppressWarnings("unused")
 public abstract class GemfireUtils extends RegionUtils {
+  public final static String GEMFIRE_NAME = new GemFireVersion().getName();
+  public final static String GEMFIRE_VERSION = CacheFactory.getVersion();
+  private static final String ASYNC_EVENT_QUEUE_ELEMENT_NAME = "async-event-queue";
+  private static final String ASYNC_EVENT_QUEUE_TYPE_NAME =
+      "org.apache.geode.cache.asyncqueue.AsyncEventQueue";
+  private static final String CQ_ELEMENT_NAME = "cq-listener-container";
+  private static final String CQ_TYPE_NAME = "org.apache.geode.cache.query.CqQuery";
+  private static final String GATEWAY_RECEIVER_ELEMENT_NAME = "gateway-receiver";
+  private static final String GATEWAY_RECEIVER_TYPE_NAME =
+      "org.apache.geode.cache.wan.GatewayReceiverFactory";
+  private static final String GATEWAY_SENDER_ELEMENT_NAME = "gateway-sender";
+  private static final String GATEWAY_SENDER_TYPE_NAME =
+      "org.apache.geode.cache.wan.GatewaySenderFactory";
 
-	public final static String APACHE_GEODE_NAME = "Apache Geode";
-	public final static String GEMFIRE_NAME = apacheGeodeProductName();
-	public final static String GEMFIRE_VERSION = apacheGeodeVersion();
-	public final static String UNKNOWN = "unknown";
+  public static String gemFireProductName() {
+    return GEMFIRE_NAME;
+  }
 
-	private static final String ASYNC_EVENT_QUEUE_ELEMENT_NAME = "async-event-queue";
-	private static final String ASYNC_EVENT_QUEUE_TYPE_NAME = "org.apache.geode.cache.asyncqueue.AsyncEventQueue";
-	private static final String CQ_ELEMENT_NAME = "cq-listener-container";
-	private static final String CQ_TYPE_NAME = "org.apache.geode.cache.query.CqQuery";
-	private static final String GATEWAY_RECEIVER_ELEMENT_NAME = "gateway-receiver";
-	private static final String GATEWAY_RECEIVER_TYPE_NAME = "org.apache.geode.cache.wan.GatewayReceiverFactory";
-	private static final String GATEWAY_SENDER_ELEMENT_NAME = "gateway-sender";
-	private static final String GATEWAY_SENDER_TYPE_NAME = "org.apache.geode.cache.wan.GatewaySenderFactory";
+  public static String gemFireVersion() {
+    return GEMFIRE_VERSION;
+  }
 
-	public static String apacheGeodeProductName() {
+  public static boolean isClassAvailable(String fullyQualifiedClassName) {
+    return ClassUtils.isPresent(fullyQualifiedClassName, GemfireUtils.class.getClassLoader());
+  }
 
-		try {
-			return GemFireVersion.getProductName();
-		}
-		catch (Throwable ignore) {
-			return APACHE_GEODE_NAME;
-		}
-	}
+  public static boolean isGemfireFeatureAvailable(GemfireFeature feature) {
 
-	public static String apacheGeodeVersion() {
+    boolean featureAvailable =
+        (!GemfireFeature.AEQ.equals(feature) || isAsyncEventQueueAvailable());
 
-		try {
-			return CacheFactory.getVersion();
-		}
-		catch (Throwable ignore) {
-			return UNKNOWN;
-		}
-	}
+    featureAvailable &=
+        (!GemfireFeature.CONTINUOUS_QUERY.equals(feature) || isContinuousQueryAvailable());
+    featureAvailable &= (!GemfireFeature.WAN.equals(feature) || isGatewayAvailable());
 
-	public static boolean isClassAvailable(String fullyQualifiedClassName) {
-		return ClassUtils.isPresent(fullyQualifiedClassName, GemfireUtils.class.getClassLoader());
-	}
+    return featureAvailable;
+  }
 
-	public static boolean isGemfireFeatureAvailable(GemfireFeature feature) {
+  public static boolean isGemfireFeatureAvailable(Element element) {
 
-		boolean featureAvailable = (!GemfireFeature.AEQ.equals(feature) || isAsyncEventQueueAvailable());
+    boolean featureAvailable = (!isAsyncEventQueue(element) || isAsyncEventQueueAvailable());
 
-		featureAvailable &= (!GemfireFeature.CONTINUOUS_QUERY.equals(feature) || isContinuousQueryAvailable());
-		featureAvailable &= (!GemfireFeature.WAN.equals(feature) || isGatewayAvailable());
+    featureAvailable &= (!isContinuousQuery(element) || isContinuousQueryAvailable());
+    featureAvailable &= (!isGateway(element) || isGatewayAvailable());
 
-		return featureAvailable;
-	}
+    return featureAvailable;
+  }
 
-	public static boolean isGemfireFeatureAvailable(Element element) {
+  public static boolean isGemfireFeatureUnavailable(GemfireFeature feature) {
+    return !isGemfireFeatureAvailable(feature);
+  }
 
-		boolean featureAvailable = (!isAsyncEventQueue(element) || isAsyncEventQueueAvailable());
+  public static boolean isGemfireFeatureUnavailable(Element element) {
+    return !isGemfireFeatureAvailable(element);
+  }
 
-		featureAvailable &= (!isContinuousQuery(element) || isContinuousQueryAvailable());
-		featureAvailable &= (!isGateway(element) || isGatewayAvailable());
+  private static boolean isAsyncEventQueue(Element element) {
+    return ASYNC_EVENT_QUEUE_ELEMENT_NAME.equals(element.getLocalName());
+  }
 
-		return featureAvailable;
-	}
+  private static boolean isAsyncEventQueueAvailable() {
+    return isClassAvailable(ASYNC_EVENT_QUEUE_TYPE_NAME);
+  }
 
-	public static boolean isGemfireFeatureUnavailable(GemfireFeature feature) {
-		return !isGemfireFeatureAvailable(feature);
-	}
+  private static boolean isContinuousQuery(Element element) {
+    return CQ_ELEMENT_NAME.equals(element.getLocalName());
+  }
 
-	public static boolean isGemfireFeatureUnavailable(Element element) {
-		return !isGemfireFeatureAvailable(element);
-	}
+  private static boolean isContinuousQueryAvailable() {
+    return isClassAvailable(CQ_TYPE_NAME);
+  }
 
-	private static boolean isAsyncEventQueue(Element element) {
-		return ASYNC_EVENT_QUEUE_ELEMENT_NAME.equals(element.getLocalName());
-	}
+  private static boolean isGateway(Element element) {
 
-	private static boolean isAsyncEventQueueAvailable() {
-		return isClassAvailable(ASYNC_EVENT_QUEUE_TYPE_NAME);
-	}
+    String elementLocalName = element.getLocalName();
 
-	private static boolean isContinuousQuery(Element element) {
-		return CQ_ELEMENT_NAME.equals(element.getLocalName());
-	}
+    return (GATEWAY_RECEIVER_ELEMENT_NAME.equals(elementLocalName)
+        || GATEWAY_SENDER_ELEMENT_NAME.equals(elementLocalName));
+  }
 
-	private static boolean isContinuousQueryAvailable() {
-		return isClassAvailable(CQ_TYPE_NAME);
-	}
+  private static boolean isGatewayAvailable() {
+    return isClassAvailable(GATEWAY_SENDER_TYPE_NAME);
+  }
 
-	private static boolean isGateway(Element element) {
-
-		String elementLocalName = element.getLocalName();
-
-		return (GATEWAY_RECEIVER_ELEMENT_NAME.equals(elementLocalName)
-			|| GATEWAY_SENDER_ELEMENT_NAME.equals(elementLocalName));
-	}
-
-	private static boolean isGatewayAvailable() {
-		return isClassAvailable(GATEWAY_SENDER_TYPE_NAME);
-	}
-
-	public static void main(final String... args) {
-		System.out.printf("Product Name [%1$s] Version [%2$s]%n", GEMFIRE_NAME, GEMFIRE_VERSION);
-	}
+  public static void main(final String... args) {
+    System.out.printf("Product Name [%1$s] Version [%2$s]%n", GEMFIRE_NAME, GEMFIRE_VERSION);
+  }
 }
