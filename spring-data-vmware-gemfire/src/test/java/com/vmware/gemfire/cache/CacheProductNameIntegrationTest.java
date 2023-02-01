bc4b1b20d69df77cc04aa88afd44371abc8e8d73
Udo Kohlmeyer <udo@vmare.com>
2023-02-01 13:29:21 +1100

    Add test to verify product Name is correctly looked up

---
+++ spring-data-vmware-gemfire/src/test/java/com/vmware/gemfire/cache/CacheProductNameIntegrationTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) VMware, Inc. 2023. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.vmware.gemfire.cache;
+
+import org.apache.geode.cache.Cache;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.gemfire.GemfireUtils;
+import org.springframework.data.gemfire.config.xml.GemfireConstants;
+import org.springframework.data.gemfire.tests.integration.SpringApplicationContextIntegrationTestsSupport;
+
+import java.io.File;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CacheProductNameIntegrationTest {
+  protected Cache getCache(String configLocation) {
+
+    String baseConfigLocation =
+        File.separator.concat(getClass().getPackage().getName().replace('.', File.separatorChar));
+
+    String resolvedConfigLocation = baseConfigLocation.concat(File.separator).concat(configLocation);
+
+    ClassPathXmlApplicationContext applicationContext =
+        new ClassPathXmlApplicationContext(resolvedConfigLocation);
+
+    return applicationContext.getBean(GemfireConstants.DEFAULT_GEMFIRE_CACHE_NAME, Cache.class);
+  }
+
+  @Test
+  public void testCacheCanRetrieveProductName() {
+    System.setProperty("logback.log.level", "info");
+    Cache cache = getCache(
+        "cacheCanResolveProductNameIntegrationTests.xml");
+    assertThat(GemfireUtils.apacheGeodeProductName()).isEqualTo("VMware GemFire");
+  }
+}
