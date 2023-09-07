5d1d897c04cafa9b6e78748632ef33298a53c803
Patrick Johnson <jpatrick@vmware.com>
2023-09-07 14:34:13 -0700

    SDGF-126: Add support for new pool methods.

--- spring-data-vmware-gemfire/src/test/java/org/springframework/data/gemfire/config/annotation/PoolPropertiesIntegrationTests.java
+++ spring-data-vmware-gemfire/src/test/java/org/springframework/data/gemfire/config/annotation/PoolPropertiesIntegrationTests.java
@@ -4,19 +4,11 @@
  */
 package org.springframework.data.gemfire.config.annotation;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-
-import java.net.InetSocketAddress;
-import java.util.function.Function;
-
-import org.junit.Test;
-
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.Pool;
 import org.apache.geode.cache.client.PoolFactory;
 import org.apache.geode.cache.client.SocketFactory;
-
+import org.junit.Test;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.MutablePropertySources;
@@ -24,6 +16,12 @@ import org.springframework.core.env.PropertySource;
 import org.springframework.data.gemfire.tests.integration.SpringApplicationContextIntegrationTestsSupport;
 import org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects;
 import org.springframework.mock.env.MockPropertySource;
+
+import java.net.InetSocketAddress;
+import java.util.function.Function;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  * Integration Tests for {@link EnablePool} and {@link EnablePools}.
@@ -62,11 +60,11 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 	}
 
 	private void assertPool(Pool pool, int freeConnectionTimeout, long idleTimeout, int loadConditioningInterval,
-			int maxConnections, int minConnections, boolean multiUserAuthentication, String name, long pingInterval,
-			boolean prSinglehopEnabled, int readTimeout, int retryAttempts, int serverConnectionTimeout,
-			String serverGroup, int socketBufferSize, int socketConnectTimeout, SocketFactory socketFactory,
-			int statisticInterval, int subscriptionAckInterval, boolean subscriptionEnabled,
-			int subscriptionMessageTrackingTimeout, int subscriptionRedundancy) {
+			int maxConnections, int minConnections, int maxConnectionsPerServer, int minConnectionsPerServer,
+			boolean multiUserAuthentication, String name, long pingInterval, boolean prSinglehopEnabled,
+			int readTimeout, int retryAttempts, int serverConnectionTimeout, String serverGroup, int socketBufferSize,
+			int socketConnectTimeout, SocketFactory socketFactory, int statisticInterval, int subscriptionAckInterval,
+			boolean subscriptionEnabled, int subscriptionMessageTrackingTimeout, int subscriptionRedundancy) {
 
 		assertThat(pool).isNotNull();
 		assertThat(pool.getFreeConnectionTimeout()).isEqualTo(freeConnectionTimeout);
@@ -74,6 +72,8 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 		assertThat(pool.getLoadConditioningInterval()).isEqualTo(loadConditioningInterval);
 		assertThat(pool.getMaxConnections()).isEqualTo(maxConnections);
 		assertThat(pool.getMinConnections()).isEqualTo(minConnections);
+		assertThat(pool.getMaxConnectionsPerServer()).isEqualTo(maxConnectionsPerServer);
+		assertThat(pool.getMinConnectionsPerServer()).isEqualTo(minConnectionsPerServer);
 		assertThat(pool.getMultiuserAuthentication()).isEqualTo(multiUserAuthentication);
 		assertThat(pool.getName()).isEqualTo(name);
 		assertThat(pool.getPingInterval()).isEqualTo(pingInterval);
@@ -100,6 +100,8 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 			.withProperty("spring.data.gemfire.pool.locators", "skullbox[11235]")
 			.withProperty("spring.data.gemfire.pool.max-connections", 400)
 			.withProperty("spring.data.gemfire.pool.min-connections", 10)
+				.withProperty("spring.data.gemfire.pool.max-connections-per-server", 40)
+				.withProperty("spring.data.gemfire.pool.min-connections-per-server", 2)
 			.withProperty("spring.data.gemfire.pool.ping-interval", 5000L)
 			.withProperty("spring.data.gemfire.pool.pr-single-hop-enabled", false)
 			.withProperty("spring.data.gemfire.pool.read-timeout", 15000)
@@ -127,6 +129,8 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 		assertThat(testPool.getLocators()).contains(new InetSocketAddress("skullbox", 11235));
 		assertThat(testPool.getMaxConnections()).isEqualTo(400);
 		assertThat(testPool.getMinConnections()).isEqualTo(10);
+		assertThat(testPool.getMaxConnectionsPerServer()).isEqualTo(40);
+		assertThat(testPool.getMinConnectionsPerServer()).isEqualTo(2);
 		assertThat(testPool.getMultiuserAuthentication()).isFalse();
 		assertThat(testPool.getName()).isEqualTo("TestPool");
 		assertThat(testPool.getPingInterval()).isEqualTo(5000L);
@@ -209,7 +213,9 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 			.withProperty("spring.data.gemfire.pool.TestPoolTwo.subscription-ack-interval", 500)
 			.withProperty("spring.data.gemfire.pool.TestPoolTwo.subscription-enabled", true)
 			.withProperty("spring.data.gemfire.pool.TestPoolTwo.subscription-message-tracking-timeout", 300000)
-			.withProperty("spring.data.gemfire.pool.TestPoolTwo.subscription-redundancy", 4);
+			.withProperty("spring.data.gemfire.pool.TestPoolTwo.subscription-redundancy", 4)
+			.withProperty("spring.data.gemfire.pool.TestPoolThree.max-connections-per-server", 300)
+			.withProperty("spring.data.gemfire.pool.TestPoolThree.min-connections-per-server", 3);
 
 		newApplicationContext(testPropertySource, TestPoolsConfiguration.class);
 
@@ -231,7 +237,7 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 		assertThat(mockSocketFactoryOne).isNotSameAs(mockSocketFactoryTwo);
 
 		assertPool(defaultPool, 15000, 20000L, 180000,
-			275, 27, true, "DEFAULT", 15000L,
+			275, 27, -1, 1, true, "DEFAULT", 15000L,
 			false, 2000, 1, 60000, "testDefaultGroup",
 			16384, 10000, SocketFactory.DEFAULT, 500, 250,
 			true, 300000, 3);
@@ -239,7 +245,7 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 		Pool testPoolOne = getBean("TestPoolOne", Pool.class);
 
 		assertPool(testPoolOne, 30000, 300000L, 120000,
-			500, 50, true, "TestPoolOne", 5000L,
+			500, 50, -1, 1, true, "TestPoolOne", 5000L,
 			false, 15000, 2, 30000, "testGroup",
 			8192, 5000, mockSocketFactoryOne,1000, 5000,
 			true, 180000, 2);
@@ -247,10 +253,18 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 		Pool testPoolTwo = getBean("TestPoolTwo", Pool.class);
 
 		assertPool(testPoolTwo, 20000, 15000L, 60000,
-			1000, 100, true, "TestPoolTwo", 20000L,
+			1000, 100, -1, 1, true, "TestPoolTwo", 20000L,
 			false, 5000, 4, 30000, "testTwoGroup",
 			65536, 15000, mockSocketFactoryTwo, 2000, 500,
 			true, 300000, 4);
+
+		Pool testPoolThree = getBean("TestPoolThree", Pool.class);
+
+		assertPool(testPoolTwo, 20000, 15000L, 60000,
+				1000, 100, -1, 1, true, "TestPoolTwo", 20000L,
+				false, 5000, 4, 30000, "testTwoGroup",
+				65536, 15000, mockSocketFactoryTwo, 2000, 500,
+				true, 300000, 4);
 	}
 
 	@EnableGemFireMockObjects
@@ -272,12 +286,32 @@ public class PoolPropertiesIntegrationTests extends SpringApplicationContextInte
 			return mock(SocketFactory.class);
 		}
 	}
+	@EnableGemFireMockObjects
+	@ClientCacheApplication
+	@EnablePool(name = "TestPool", idleTimeout = 10000L, maxConnectionsPerServer = 250, minConnectionsPerServer = 25,
+			serverConnectionTimeout = 60000, socketFactoryBeanName = "mockSocketFactory")
+	static class PerServerTestPoolConfiguration {
+
+		@Bean
+		PoolConfigurer testPoolConfigurer() {
+			return (beanName, beanFactory) -> {
+				beanFactory.setLoadConditioningInterval(100000);
+				beanFactory.setRetryAttempts(1);
+			};
+		}
+
+		@Bean
+		SocketFactory mockSocketFactory() {
+			return mock(SocketFactory.class);
+		}
+	}
 
 	@EnableGemFireMockObjects
 	@ClientCacheApplication
 	@EnablePools(pools = {
 		@EnablePool(name = "TestPoolOne"),
-		@EnablePool(name = "TestPoolTwo")
+		@EnablePool(name = "TestPoolTwo"),
+		@EnablePool(name = "TestPoolThree")
 	})
 	static class TestPoolsConfiguration {
 
