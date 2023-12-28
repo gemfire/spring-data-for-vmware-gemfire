3293b3a6dfad4166de5d330ba1a3da6045e605c5
Patrick Johnson <jpatrick@vmware.com>
2023-12-28 11:54:08 -0800

    SDGF-189: Migrate Gradle to Kotlin DSL, upgrade to 8.5, and introduce version catalogs.

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+// Copyright (c) VMware, Inc. 2022. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
+buildscript {
+  repositories {
+    mavenCentral()
+  }
+}
+
+plugins {
+  id("java-library")
+  alias(libs.plugins.versions)
+  alias(libs.plugins.version.catalog.update)
+}
+
+allprojects {
+  repositories {
+    mavenCentral()
+    maven {
+      credentials {
+        username = property("gemfireRepoUsername") as String
+        password = property("gemfireRepoPassword") as String
+      }
+      url = uri("https://commercial-repo.pivotal.io/data3/gemfire-release-repo/gemfire")
+    }
+  }
+}
+
+versionCatalogUpdate {
+  // These options will be set as default for all version catalogs
+  sortByKey = true
+  // Referenced that are pinned are not automatically updated.
+  // They are also not automatically kept however (use keep for that).
+  pin {
+  }
+  keep {
+    keepUnusedVersions = true
+    // keep all libraries that aren't used in the project
+    keepUnusedLibraries = true
+    // keep all plugins that aren't used in the project
+    keepUnusedPlugins = true
+  }
+
+  versionCatalogs {
+    create("publishCatalog"){
+      catalogFile = file("gradle/publishing.versions.toml")
+    }
+  }
+}
+
+tasks.withType<DependencyUpdatesTask> {
+  rejectVersionIf {
+    !isPatch(candidate.version, currentVersion)
+  }
+}
+
+fun isPatch(candidateVersion: String, currentVersion: String): Boolean {
+  val candidateSplit = candidateVersion.split(".")
+  val currentSplit = currentVersion.split(".")
+
+  if (candidateSplit.size == currentSplit.size && currentSplit.size == 3) {
+    if (candidateSplit[0] != currentSplit[0]) {
+      return false
+    }
+    if (candidateSplit[1] != currentSplit[1]) {
+      return false
+    }
+  }
+  return true
+}
