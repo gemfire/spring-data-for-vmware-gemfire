4dc7e8abf31d80789a888eeacd107d816879b853
Udo Kohlmeyer <udo@vmware.com>
2024-06-21 20:20:47 +0200

    SDGF-287: Adding commercial repositories support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,9 @@
 import java.io.FileInputStream
 import java.util.*
 
-// Copyright (c) VMware, Inc. 2022. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 pluginManagement {
   includeBuild("build-tools/publishing")
+  includeBuild("build-tools/convention-plugins")
   repositories {
     maven { url = uri("https://repo.spring.io/milestone") }
     gradlePluginPortal()
