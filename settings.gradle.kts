8103f2f13ac3a35bba4a72252a00a648063b805a
Udo Kohlmeyer <udo@vmware.com>
2024-08-30 19:44:00 +0200

    SDGF-294: Add publishing to Artifactory

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,6 @@
 import java.io.FileInputStream
 import java.util.*
 
-// Copyright (c) VMware, Inc. 2022. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 pluginManagement {
   includeBuild("build-tools/publishing")
   includeBuild("build-tools/convention-plugins")
@@ -19,6 +16,10 @@ pluginManagement {
 }
 
 include("spring-data-vmware-gemfire")
+
+project(":spring-data-vmware-gemfire").name = "spring-data-vmware-gemfire"
+
+rootProject.name = "spring-data-for-vmware-gemfire"
 
 dependencyResolutionManagement {
   versionCatalogs {
