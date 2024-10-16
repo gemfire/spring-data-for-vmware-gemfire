7e241b2d999ce6c05f95a6ae707052194f461eec
Patrick Johnson <patrick.johnson@broadcom.com>
2024-10-16 16:40:58 -0700

    SDGF-315: Fix artifact name.

--- spring-test-vmware-gemfire/build.gradle.kts
+++ spring-test-vmware-gemfire/build.gradle.kts
@@ -30,9 +30,9 @@ tasks.named<Javadoc>("javadoc") {
 }
 
 publishingDetails {
-    artifactName.set("spring-data-3.3-gemfire-${getGemFireBaseVersion()}")
-    longName.set("Spring Test Framework for VMware GemFire ${getGemFireBaseVersion()} and Spring Data 3.3")
-    description.set("Spring Test Framework for VMware GemFire ${getGemFireBaseVersion()} and Spring Data 3.3")
+    artifactName.set("spring-data-3.1-gemfire-${getGemFireBaseVersion()}")
+    longName.set("Spring Test Framework for VMware GemFire ${getGemFireBaseVersion()} and Spring Data 3.1")
+    description.set("Spring Test Framework for VMware GemFire ${getGemFireBaseVersion()} and Spring Data 3.1")
     test.set(true)
 }
 
