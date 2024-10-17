bfa4ac4e69c7ca35411da3a6ee3f181d6bf85ae6
Udo Kohlmeyer <udo@vmware.com>
2024-10-18 08:58:30 +1100

    SDGF-315: Force dependencies on publishToInternalGCS tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,4 +81,6 @@ fun isPatch(candidateVersion: String, currentVersion: String): Boolean {
 
 gradle.projectsEvaluated {
   project(":spring-data-vmware-gemfire").tasks["publish"].mustRunAfter(project(":spring-test-vmware-gemfire").tasks["publish"])
+  project(":spring-data-vmware-gemfire").tasks["publishToMavenLocal"].mustRunAfter(project(":spring-test-vmware-gemfire").tasks["publishToMavenLocal"])
+  project(":spring-data-vmware-gemfire").tasks["publishToInternalGCS"].mustRunAfter(project(":spring-test-vmware-gemfire").tasks["publishToInternalGCS"])
 }
