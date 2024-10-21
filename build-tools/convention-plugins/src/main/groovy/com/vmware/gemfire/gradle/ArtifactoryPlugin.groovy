5d833c240d56b4f22772ab32b3f93ed03c089d30
Udo Kohlmeyer <udo@vmware.com>
2024-10-22 09:18:07 +1100

    SDGF-315: Update artifactory build name

--- build-tools/convention-plugins/src/main/groovy/com/vmware/gemfire/gradle/ArtifactoryPlugin.groovy
+++ build-tools/convention-plugins/src/main/groovy/com/vmware/gemfire/gradle/ArtifactoryPlugin.groovy
@@ -34,7 +34,7 @@ class ArtifactoryPlugin implements Plugin<Project> {
         // clientConfig.setIncludeEnvVars(true)
 
         def buildInfo = clientConfig.info
-        buildInfo.setBuildName("spring-data-3.3-gemfire-10.1")
+        buildInfo.setBuildName("spring-data-3.3-gemfire-10.1-1.0.x")
         buildInfo.setBuildNumber(project.properties.getOrDefault("buildId", '0000') as String)
         buildInfo.setProject("tds-gemfire")
         buildInfo.setAgentName(System.properties['user.name'] as String)
