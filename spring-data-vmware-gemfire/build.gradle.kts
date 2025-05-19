1fd9bbefdfa8ce22bc90d7ec0a68e3c5c5e653ee
Udo Kohlmeyer <udo.kohlmeyer@broadcom.com>
2025-05-20 08:59:23 +1000

    Fix artifact number

--- spring-data-vmware-gemfire/build.gradle.kts
+++ spring-data-vmware-gemfire/build.gradle.kts
@@ -65,7 +65,7 @@ tasks.withType<JavaCompile>().configureEach {
 }
 
 publishingDetails {
-  artifactName.set("spring-data-3.4-gemfire-${getGemFireBaseVersion()}")
+  artifactName.set("spring-data-3.5-gemfire-${getGemFireBaseVersion()}")
   longName.set("Spring Data VMware GemFire")
   description.set("Spring Data For VMware GemFire")
   test.set(false)
