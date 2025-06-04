f03b9ea2ee8a49eea281595bc8542ce78bf6ae24
Udo Kohlmeyer <udo.kohlmeyer@broadcom.com>
2025-06-05 08:10:27 +1000

    Revert "GEM-14776: Update artifact from 3.3 to 3.5"
    This reverts commit 35bd4652695ec3d7ac7a75c7ea32dea13306964e.

--- spring-data-vmware-gemfire/build.gradle.kts
+++ spring-data-vmware-gemfire/build.gradle.kts
@@ -50,7 +50,7 @@ tasks.withType<JavaCompile>().configureEach {
 }
 
 publishingDetails {
-  artifactName.set("spring-data-3.5-gemfire-${getGemFireBaseVersion()}")
+  artifactName.set("spring-data-3.3-gemfire-${getGemFireBaseVersion()}")
   longName.set("Spring Data VMware GemFire")
   description.set("Spring Data For VMware GemFire")
   test.set(false)
