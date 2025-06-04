b58b971cee01562d2004cde714909cd0d35cb7a8
Udo Kohlmeyer <udo.kohlmeyer@broadcom.com>
2025-06-05 09:08:32 +1000

    GEM-14776: Fix artifactory name

--- build-tools/convention-plugins/src/main/groovy/com/vmware/gemfire/gradle/ArtifactoryPlugin.groovy
+++ build-tools/convention-plugins/src/main/groovy/com/vmware/gemfire/gradle/ArtifactoryPlugin.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Broadcom. All rights reserved.
+ * Copyright 2024-2025 Broadcom. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -34,7 +34,7 @@ class ArtifactoryPlugin implements Plugin<Project> {
         // clientConfig.setIncludeEnvVars(true)
 
         def buildInfo = clientConfig.info
-        buildInfo.setBuildName(project.name + "-" + project.version)
+        buildInfo.setBuildName("spring-data-3.5-gemfire-10.1-2.0.x")
         buildInfo.setBuildNumber(project.properties.getOrDefault("buildId", '0000') as String)
         buildInfo.setProject("tds-gemfire")
         buildInfo.setAgentName(System.properties['user.name'] as String)
