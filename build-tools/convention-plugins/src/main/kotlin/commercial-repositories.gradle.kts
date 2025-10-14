86d86a5d782e6feb1b8055ae4face0d1490f9f4e
Udo Kohlmeyer <udo.kohlmeyer@broadcom.com>
2025-10-14 11:35:28 +1100

    Bumping to latest Spring milestone releases

--- build-tools/convention-plugins/src/main/kotlin/commercial-repositories.gradle.kts
+++ build-tools/convention-plugins/src/main/kotlin/commercial-repositories.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Broadcom. All rights reserved.
+ * Copyright 2024-2025 Broadcom. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -17,19 +17,10 @@ repositories {
     providers.environmentVariable("HOME").get() + "/.gradle/gradleRepositories.json"
   )
 
-  val enablePrivateCommercialRepos =
-    providers.gradleProperty("spring.gemfire.enable.private.repositories")
-      .getOrElse("false").toBoolean()
   val jsonString = File(repositoryConfigFilePath).readText(Charsets.UTF_8)
   val repositories = groovy.json.JsonSlurper().parseText(jsonString) as Map<*, *>
   (repositories["repositories"] as List<*>).filterNotNull().map { entry -> entry as Map<*, *> }
-    .filter { entry ->
-      return@filter if (entry["private"]!! as Boolean) {
-        enablePrivateCommercialRepos
-      } else {
-        true
-      }
-    }.forEach { entry ->
+    .forEach { entry ->
       entry.apply {
         maven {
           url = uri(entry["url"]!! as String)
